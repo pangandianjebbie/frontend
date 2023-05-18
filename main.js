e18b6f0298a2e5817844533864bf59578250609a
unknown <jebbieannepangandian@gmail.com>
2023-05-18 12:37:30 +0800

    Added Axios Functionality to OpenAI

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path'); 
+const axios = require('axios');
 
 const isDev = true;
 
@@ -20,7 +21,9 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
-  createWindow()
+  ipcMain.handle('axios.openAI', openAI)
+
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -34,3 +37,32 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 });
+
+async function openAI(e, ingredients){
+  let res = null;
+
+  await axios({
+    method: 'post',
+    url: 'https://api.openai.com/v1/completions',
+    data: {
+      model: "text-davinci-003",
+      prompt: "Write a recipe based on these ingredients and instructions:\n\n " + ingredients,
+      temperature: 0.3,
+      max_tokens: 120,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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'
+    }
+  }).then(function (response) {
+    res = response.data;
+  })
+  .catch(function (error) {
+    res = error;
+  });
+
+  return res;
+}
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,2 +1,17 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+//const information = document.getElementById('info')
+//information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+
+const form = document.getElementById("form_recipe");
+if (form) {
+    form.onsubmit = async function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        const response = await window.axios.openAI(formData.get("ingredients"));  
+        document.getElementById("recipe_instructions").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
+        //console.log();
+ 
+    };
+}
